hanzoai/dashboards

commit 4fd96747a4a1a6805740e4c30f99607c15942556

Fix css loading in plugins (#12573)
- allow css loader to be imported again (wasnt prefixed by plugin)

public/app/features/plugins/plugin_loader.ts
@@ -56,7 +56,7 @@ System.config({
     css: 'vendor/plugin-css/css.js',
   },
   meta: {
-    'plugin*': {
+    '*': {
       esModule: true,
       authorization: true,
       loader: 'plugin-loader',

commit c4c7be5f5fa49be026176f49051bfbfd6222427a

Fix freezing browser when loading plugin
- broken since 4fd96747a
- `*` was previously working as a path matcher, but freezes browser when
  used with new cache-busting plugin loader
- changed matcher to be `/*`

public/app/features/plugins/plugin_loader.ts
@@ -56,7 +56,7 @@ System.config({
     css: 'vendor/plugin-css/css.js',
   },
   meta: {
-    '*': {
+    '/*': {
       esModule: true,
       authorization: true,
       loader: 'plugin-loader',