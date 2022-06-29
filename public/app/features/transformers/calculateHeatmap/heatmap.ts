hanzoai/dashboards

commit 8e5e29ac924e8b554f951a91c6044d255911c6d2

HeatmapNG: consolidate frame types & fix color ranging (#51089)
* rename yZeroDisplay -> yMinDisplay
* remove heatmap-cells-sparse frame type
* parse x bucket size to millis
* take into account hideLE & hideGE filters to auto-range color scale
* extract cell value range scanning to heatmapData

public/app/features/transformers/calculateHeatmap/heatmap.ts
@@ -13,6 +13,8 @@ import {
   Field,
   getValueFormat,
   formattedValueToString,
+  durationToMilliseconds,
+  parseDuration,
 } from '@grafana/data';
 import { ScaleDistribution } from '@grafana/schema';
 
@@ -56,12 +58,32 @@ export interface HeatmapRowsCustomMeta {
   yOrdinalDisplay: string[];
   yOrdinalLabel?: string[];
   yMatchWithLabel?: string;
-  yZeroDisplay?: string;
+  yMinDisplay?: string;
 }
 
 /** simple utility to get heatmap metadata from a frame */
 export function readHeatmapRowsCustomMeta(frame?: DataFrame): HeatmapRowsCustomMeta {
   return (frame?.meta?.custom ?? {}) as HeatmapRowsCustomMeta;
+}
+
+export function isHeatmapCellsDense(frame: DataFrame) {
+  let foundY = false;
+
+  for (let field of frame.fields) {
+    // dense heatmap frames can only have one of these fields
+    switch (field.name) {
+      case 'y':
+      case 'yMin':
+      case 'yMax':
+        if (foundY) {
+          return false;
+        }
+
+        foundY = true;
+    }
+  }
+
+  return foundY;
 }
 
 export interface RowsHeatmapOptions {
@@ -129,7 +151,7 @@ export function rowsToCellsHeatmap(opts: RowsHeatmapOptions): DataFrame {
   if (custom.yMatchWithLabel) {
     custom.yOrdinalLabel = yFields.map((f) => f.labels?.[custom.yMatchWithLabel!] ?? '');
     if (custom.yMatchWithLabel === 'le') {
-      custom.yZeroDisplay = '0.0';
+      custom.yMinDisplay = '0.0';
     }
   }
 
@@ -137,8 +159,8 @@ export function rowsToCellsHeatmap(opts: RowsHeatmapOptions): DataFrame {
   // TODO: this leaves the internally prepended '0.0' without this formatting treatment
   if (opts.unit?.length || opts.decimals != null) {
     const fmt = getValueFormat(opts.unit ?? 'short');
-    if (custom.yZeroDisplay) {
-      custom.yZeroDisplay = formattedValueToString(fmt(0, opts.decimals));
+    if (custom.yMinDisplay) {
+      custom.yMinDisplay = formattedValueToString(fmt(0, opts.decimals));
     }
     custom.yOrdinalDisplay = custom.yOrdinalDisplay.map((name) => {
       let num = +name;
@@ -274,11 +296,12 @@ export function calculateHeatmapFromData(frames: DataFrame[], options: HeatmapCa
   const scaleDistribution = options.yBuckets?.scale ?? {
     type: ScaleDistribution.Linear,
   };
+
   const heat2d = heatmap(xs, ys, {
     xSorted: true,
     xTime: xField.type === FieldType.time,
     xMode: xBucketsCfg.mode,
-    xSize: xBucketsCfg.value ? +xBucketsCfg.value : undefined,
+    xSize: durationToMilliseconds(parseDuration(xBucketsCfg.value ?? '')),
     yMode: yBucketsCfg.mode,
     ySize: yBucketsCfg.value ? +yBucketsCfg.value : undefined,
     yLog: scaleDistribution?.type === ScaleDistribution.Log ? (scaleDistribution?.log as any) : undefined,

public/app/plugins/panel/heatmap-new/utils.ts
@@ -17,7 +17,7 @@ import {
 } from '@grafana/data';
 import { AxisPlacement, ScaleDirection, ScaleDistribution, ScaleOrientation } from '@grafana/schema';
 import { UPlotConfigBuilder } from '@grafana/ui';
-import { readHeatmapRowsCustomMeta } from 'app/features/transformers/calculateHeatmap/heatmap';
+import { isHeatmapCellsDense, readHeatmapRowsCustomMeta } from 'app/features/transformers/calculateHeatmap/heatmap';
 import { HeatmapCellLayout } from 'app/features/transformers/calculateHeatmap/models.gen';
 
 import { pointWithin, Quadtree, Rect } from '../barchart/quadtree';
@@ -72,8 +72,6 @@ interface PrepConfigOpts {
   cellGap?: number | null; // in css pixels
   hideLE?: number;
   hideGE?: number;
-  valueMin?: number;
-  valueMax?: number;
   yAxisConfig: YAxisConfig;
   ySizeDivisor?: number;
   sync?: () => DashboardCursorSync;
@@ -94,8 +92,6 @@ export function prepConfig(opts: PrepConfigOpts) {
     cellGap,
     hideLE,
     hideGE,
-    valueMin,
-    valueMax,
     yAxisConfig,
     ySizeDivisor,
     sync,
@@ -264,7 +260,8 @@ export function prepConfig(opts: PrepConfigOpts) {
   const yFieldConfig = yField.config?.custom as PanelFieldConfig | undefined;
   const yScale = yFieldConfig?.scaleDistribution ?? { type: ScaleDistribution.Linear };
   const yAxisReverse = Boolean(yAxisConfig.reverse);
-  const shouldUseLogScale = yScale.type !== ScaleDistribution.Linear || heatmapType === DataFrameType.HeatmapSparse;
+  const isSparseHeatmap = heatmapType === DataFrameType.HeatmapCells && !isHeatmapCellsDense(dataRef.current?.heatmap!);
+  const shouldUseLogScale = yScale.type !== ScaleDistribution.Linear || isSparseHeatmap;
   const isOrdianalY = readHeatmapRowsCustomMeta(dataRef.current?.heatmap).yOrdinalDisplay != null;
 
   // random to prevent syncing y in other heatmaps
@@ -282,7 +279,7 @@ export function prepConfig(opts: PrepConfigOpts) {
     log: yScale.log ?? 2,
     range:
       // sparse already accounts for le/ge by explicit yMin & yMax cell bounds, so no need to expand y range
-      heatmapType === DataFrameType.HeatmapSparse
+      isSparseHeatmap
         ? (u, dataMin, dataMax) => {
             let scaleMin: number | null, scaleMax: number | null;
 
@@ -432,7 +429,7 @@ export function prepConfig(opts: PrepConfigOpts) {
           if (meta.yOrdinalDisplay) {
             return splits.map((v) =>
               v < 0
-                ? meta.yZeroDisplay ?? '' // Check prometheus style labels
+                ? meta.yMinDisplay ?? '' // Check prometheus style labels
                 : meta.yOrdinalDisplay[v] ?? ''
             );
           }
@@ -441,7 +438,7 @@ export function prepConfig(opts: PrepConfigOpts) {
       : undefined,
   });
 
-  const pathBuilder = heatmapType === DataFrameType.HeatmapCells ? heatmapPathsDense : heatmapPathsSparse;
+  const pathBuilder = isSparseHeatmap ? heatmapPathsSparse : heatmapPathsDense;
 
   // heatmap layer
   builder.addSeries({
@@ -485,8 +482,13 @@ export function prepConfig(opts: PrepConfigOpts) {
       disp: {
         fill: {
           values: (u, seriesIdx) => {
-            let countFacetIdx = heatmapType === DataFrameType.HeatmapCells ? 2 : 3;
-            return valuesToFills(u.data[seriesIdx][countFacetIdx] as unknown as number[], palette, valueMin, valueMax);
+            let countFacetIdx = !isSparseHeatmap ? 2 : 3;
+            return valuesToFills(
+              u.data[seriesIdx][countFacetIdx] as unknown as number[],
+              palette,
+              dataRef.current?.minValue!,
+              dataRef.current?.maxValue!
+            );
           },
           index: palette,
         },
@@ -886,12 +888,20 @@ export function heatmapPathsSparse(opts: PathbuilderOpts) {
   };
 }
 
-export const valuesToFills = (values: number[], palette: string[], minValue?: number, maxValue?: number) => {
+export const boundedMinMax = (
+  values: number[],
+  minValue?: number,
+  maxValue?: number,
+  hideLE = -Infinity,
+  hideGE = Infinity
+) => {
   if (minValue == null) {
     minValue = Infinity;
 
     for (let i = 0; i < values.length; i++) {
-      minValue = Math.min(minValue, values[i]);
+      if (values[i] > hideLE && values[i] < hideGE) {
+        minValue = Math.min(minValue, values[i]);
+      }
     }
   }
 
@@ -899,10 +909,16 @@ export const valuesToFills = (values: number[], palette: string[], minValue?: nu
     maxValue = -Infinity;
 
     for (let i = 0; i < values.length; i++) {
-      maxValue = Math.max(maxValue, values[i]);
+      if (values[i] > hideLE && values[i] < hideGE) {
+        maxValue = Math.max(maxValue, values[i]);
+      }
     }
   }
 
+  return [minValue, maxValue];
+};
+
+export const valuesToFills = (values: number[], palette: string[], minValue: number, maxValue: number) => {
   let range = maxValue - minValue;
 
   let paletteSize = palette.length;

packages/grafana-data/src/types/dataFrameTypes.ts
@@ -26,11 +26,4 @@ export enum DataFrameType {
    * If the y value is actually ordinal, use `meta.custom` to specify the bucket lookup values
    */
   HeatmapCells = 'heatmap-cells',
-
-  /**
-   *  WIP sparse heatmap support
-   *
-   *  @private
-   */
-  HeatmapSparse = 'heatmap-cells-sparse',
 }

pkg/util/converter/prom.go
@@ -429,7 +429,7 @@ func readMatrixOrVectorWide(iter *jsoniter.Iterator, resultType string) *backend
 			histogram.yMin.Labels = valueField.Labels
 			frame := data.NewFrame(valueField.Name, histogram.time, histogram.yMin, histogram.yMax, histogram.count, histogram.yLayout)
 			frame.Meta = &data.FrameMeta{
-				Type: "heatmap-cells-sparse",
+				Type: "heatmap-cells",
 			}
 			if frame.Name == data.TimeSeriesValueFieldName {
 				frame.Name = "" // only set the name if useful
@@ -535,7 +535,7 @@ func readMatrixOrVectorMulti(iter *jsoniter.Iterator, resultType string) *backen
 			histogram.yMin.Labels = valueField.Labels
 			frame := data.NewFrame(valueField.Name, histogram.time, histogram.yMin, histogram.yMax, histogram.count, histogram.yLayout)
 			frame.Meta = &data.FrameMeta{
-				Type: "heatmap-cells-sparse",
+				Type: "heatmap-cells",
 			}
 			if frame.Name == data.TimeSeriesValueFieldName {
 				frame.Name = "" // only set the name if useful

pkg/util/converter/testdata/prom-matrix-histogram-no-labels-frame.jsonc
@@ -1,9 +1,9 @@
 //  🌟 This was machine generated.  Do not edit. 🌟
-//  
+//
 //  Frame[0] {
-//      "type": "heatmap-cells-sparse"
+//      "type": "heatmap-cells"
 //  }
-//  Name: 
+//  Name:
 //  Dimensions: 5 Fields by 932 Rows
 //  +-------------------------------+------------------------+------------------------+--------------------+---------------+
 //  | Name: xMax                    | Name: yMin             | Name: yMax             | Name: count        | Name: yLayout |
@@ -21,15 +21,15 @@
 //  | 2022-04-14 19:08:20 +0000 UTC | 9.07293025972535e-06   | 9.894100606163098e-06  | 3.712286008031362  | 0             |
 //  | ...                           | ...                    | ...                    | ...                | ...           |
 //  +-------------------------------+------------------------+------------------------+--------------------+---------------+
-//  
-//  
+//
+//
 //  🌟 This was machine generated.  Do not edit. 🌟
 {
   "frames": [
     {
       "schema": {
         "meta": {
-          "type": "heatmap-cells-sparse"
+          "type": "heatmap-cells"
         },
         "fields": [
           {

pkg/util/converter/testdata/prom-matrix-histogram-no-labels-wide-frame.jsonc
@@ -1,9 +1,9 @@
 //  🌟 This was machine generated.  Do not edit. 🌟
-//  
+//
 //  Frame[0] {
-//      "type": "heatmap-cells-sparse"
+//      "type": "heatmap-cells"
 //  }
-//  Name: 
+//  Name:
 //  Dimensions: 5 Fields by 932 Rows
 //  +-------------------------------+------------------------+------------------------+--------------------+---------------+
 //  | Name: xMax                    | Name: yMin             | Name: yMax             | Name: count        | Name: yLayout |
@@ -21,15 +21,15 @@
 //  | 2022-04-14 19:08:20 +0000 UTC | 9.07293025972535e-06   | 9.894100606163098e-06  | 3.712286008031362  | 0             |
 //  | ...                           | ...                    | ...                    | ...                | ...           |
 //  +-------------------------------+------------------------+------------------------+--------------------+---------------+
-//  
-//  
+//
+//
 //  🌟 This was machine generated.  Do not edit. 🌟
 {
   "frames": [
     {
       "schema": {
         "meta": {
-          "type": "heatmap-cells-sparse"
+          "type": "heatmap-cells"
         },
         "fields": [
           {

commit 061b31319b676088395cb4096c6246b1efa684a6

HeatmapNG: expand log y axis min/max config to full powers (#51472)
also fix x bucket count setting for calculate (regressed in #51089)

Co-authored-by: Ryan McKinley <ryantxu@gmail.com>

.betterer.results
@@ -7071,10 +7071,10 @@ exports[`better eslint`] = {
     "public/app/features/transformers/calculateHeatmap/editor/helper.ts:2898656130": [
       [10, 37, 3, "Unexpected any. Specify a different type.", "193409811"]
     ],
-    "public/app/features/transformers/calculateHeatmap/heatmap.ts:992367420": [
+    "public/app/features/transformers/calculateHeatmap/heatmap.ts:3354697941": [
       [65, 9, 52, "Do not use any type assertions.", "3480700880"],
-      [321, 63, 29, "Do not use any type assertions.", "255738422"],
-      [321, 89, 3, "Unexpected any. Specify a different type.", "193409811"]
+      [326, 63, 29, "Do not use any type assertions.", "255738422"],
+      [326, 89, 3, "Unexpected any. Specify a different type.", "193409811"]
     ],
     "public/app/features/transformers/configFromQuery/ConfigFromQueryTransformerEditor.test.tsx:1060905279": [
       [44, 29, 21, "Do not use any type assertions.", "1548027068"]
@@ -11734,29 +11734,29 @@ exports[`better eslint`] = {
       [87, 39, 23, "Do not use any type assertions.", "3906212682"],
       [87, 59, 3, "Unexpected any. Specify a different type.", "193409811"]
     ],
-    "public/app/plugins/panel/heatmap/utils.ts:215809412": [
+    "public/app/plugins/panel/heatmap/utils.ts:2227722730": [
       [260, 23, 53, "Do not use any type assertions.", "4087268626"],
       [287, 49, 51, "Do not use any type assertions.", "4216242190"],
       [287, 49, 28, "Do not use any type assertions.", "1631991143"],
-      [476, 14, 190, "Do not use any type assertions.", "4120382085"],
-      [487, 14, 55, "Do not use any type assertions.", "2123957774"],
-      [487, 14, 43, "Do not use any type assertions.", "3195961945"],
-      [630, 19, 27, "Do not use any type assertions.", "3603045978"],
-      [630, 19, 15, "Do not use any type assertions.", "3099372941"],
-      [631, 19, 27, "Do not use any type assertions.", "3664280731"],
-      [631, 19, 15, "Do not use any type assertions.", "2963010956"],
-      [632, 23, 27, "Do not use any type assertions.", "1493467224"],
-      [632, 23, 15, "Do not use any type assertions.", "2572578447"],
-      [738, 25, 30, "Do not use any type assertions.", "1186575399"],
-      [738, 25, 16, "Do not use any type assertions.", "1728826198"],
-      [810, 22, 27, "Do not use any type assertions.", "3603045978"],
-      [810, 22, 15, "Do not use any type assertions.", "3099372941"],
-      [811, 22, 27, "Do not use any type assertions.", "3664280731"],
-      [811, 22, 15, "Do not use any type assertions.", "2963010956"],
-      [812, 22, 27, "Do not use any type assertions.", "1493467224"],
-      [812, 22, 15, "Do not use any type assertions.", "2572578447"],
-      [813, 23, 27, "Do not use any type assertions.", "1554701977"],
-      [813, 23, 15, "Do not use any type assertions.", "2436216462"]
+      [488, 14, 190, "Do not use any type assertions.", "4120382085"],
+      [499, 14, 55, "Do not use any type assertions.", "2123957774"],
+      [499, 14, 43, "Do not use any type assertions.", "3195961945"],
+      [642, 19, 27, "Do not use any type assertions.", "3603045978"],
+      [642, 19, 15, "Do not use any type assertions.", "3099372941"],
+      [643, 19, 27, "Do not use any type assertions.", "3664280731"],
+      [643, 19, 15, "Do not use any type assertions.", "2963010956"],
+      [644, 23, 27, "Do not use any type assertions.", "1493467224"],
+      [644, 23, 15, "Do not use any type assertions.", "2572578447"],
+      [750, 25, 30, "Do not use any type assertions.", "1186575399"],
+      [750, 25, 16, "Do not use any type assertions.", "1728826198"],
+      [822, 22, 27, "Do not use any type assertions.", "3603045978"],
+      [822, 22, 15, "Do not use any type assertions.", "3099372941"],
+      [823, 22, 27, "Do not use any type assertions.", "3664280731"],
+      [823, 22, 15, "Do not use any type assertions.", "2963010956"],
+      [824, 22, 27, "Do not use any type assertions.", "1493467224"],
+      [824, 22, 15, "Do not use any type assertions.", "2572578447"],
+      [825, 23, 27, "Do not use any type assertions.", "1554701977"],
+      [825, 23, 15, "Do not use any type assertions.", "2436216462"]
     ],
     "public/app/plugins/panel/histogram/Histogram.tsx:3993177092": [
       [130, 31, 3, "Unexpected any. Specify a different type.", "193409811"],

public/app/features/transformers/calculateHeatmap/heatmap.ts
@@ -316,7 +316,12 @@ export function calculateHeatmapFromData(frames: DataFrame[], options: HeatmapCa
     xSorted: true,
     xTime: xField.type === FieldType.time,
     xMode: xBucketsCfg.mode,
-    xSize: durationToMilliseconds(parseDuration(xBucketsCfg.value ?? '')),
+    xSize:
+      xBucketsCfg.mode === HeatmapCalculationMode.Size
+        ? durationToMilliseconds(parseDuration(xBucketsCfg.value ?? ''))
+        : xBucketsCfg.value
+        ? +xBucketsCfg.value
+        : undefined,
     yMode: yBucketsCfg.mode,
     ySize: yBucketsCfg.value ? +yBucketsCfg.value : undefined,
     yLog: scaleDistribution?.type === ScaleDistribution.Log ? (scaleDistribution?.log as any) : undefined,

public/app/plugins/panel/heatmap/utils.ts
@@ -289,15 +289,21 @@ export function prepConfig(opts: PrepConfigOpts) {
               : [dataMin, dataMax];
 
             if (shouldUseLogScale && !isOrdianalY) {
+              let yExp = u.scales[yScaleKey].log!;
+              let log = yExp === 2 ? Math.log2 : Math.log10;
+
               let { min: explicitMin, max: explicitMax } = yAxisConfig;
 
               // guard against <= 0
               if (explicitMin != null && explicitMin > 0) {
-                scaleMin = explicitMin;
+                // snap to magnitude
+                let minLog = log(explicitMin);
+                scaleMin = yExp ** incrRoundDn(minLog, 1);
               }
 
               if (explicitMax != null && explicitMax > 0) {
-                scaleMax = explicitMax;
+                let maxLog = log(explicitMax);
+                scaleMax = yExp ** incrRoundUp(maxLog, 1);
               }
             }
 
@@ -305,6 +311,9 @@ export function prepConfig(opts: PrepConfigOpts) {
           }
         : // dense and ordinal only have one of yMin|yMax|y, so expand range by one cell in the direction of le/ge/unknown
           (u, dataMin, dataMax) => {
+            let scaleMin = dataMin,
+              scaleMax = dataMax;
+
             let { min: explicitMin, max: explicitMax } = yAxisConfig;
 
             // logarithmic expansion
@@ -314,44 +323,47 @@ export function prepConfig(opts: PrepConfigOpts) {
               let minExpanded = false;
               let maxExpanded = false;
 
-              if (ySizeDivisor !== 1) {
-                let log = yExp === 2 ? Math.log2 : Math.log10;
+              let log = yExp === 2 ? Math.log2 : Math.log10;
 
+              if (ySizeDivisor !== 1) {
                 let minLog = log(dataMin);
                 let maxLog = log(dataMax);
 
                 if (!Number.isInteger(minLog)) {
-                  dataMin = yExp ** incrRoundDn(minLog, 1);
+                  scaleMin = yExp ** incrRoundDn(minLog, 1);
                   minExpanded = true;
                 }
 
                 if (!Number.isInteger(maxLog)) {
-                  dataMax = yExp ** incrRoundUp(maxLog, 1);
+                  scaleMax = yExp ** incrRoundUp(maxLog, 1);
                   maxExpanded = true;
                 }
               }
 
               if (dataRef.current?.yLayout === HeatmapCellLayout.le) {
                 if (!minExpanded) {
-                  dataMin /= yExp;
+                  scaleMin /= yExp;
                 }
               } else if (dataRef.current?.yLayout === HeatmapCellLayout.ge) {
                 if (!maxExpanded) {
-                  dataMax *= yExp;
+                  scaleMax *= yExp;
                 }
               } else {
-                dataMin /= yExp / 2;
-                dataMax *= yExp / 2;
+                scaleMin /= yExp / 2;
+                scaleMax *= yExp / 2;
               }
 
               if (!isOrdianalY) {
                 // guard against <= 0
                 if (explicitMin != null && explicitMin > 0) {
-                  dataMin = explicitMin;
+                  // snap down to magnitude
+                  let minLog = log(explicitMin);
+                  scaleMin = yExp ** incrRoundDn(minLog, 1);
                 }
 
                 if (explicitMax != null && explicitMax > 0) {
-                  dataMax = explicitMax;
+                  let maxLog = log(explicitMax);
+                  scaleMax = yExp ** incrRoundUp(maxLog, 1);
                 }
               }
             }
@@ -365,24 +377,24 @@ export function prepConfig(opts: PrepConfigOpts) {
 
               if (bucketSize) {
                 if (dataRef.current?.yLayout === HeatmapCellLayout.le) {
-                  dataMin -= bucketSize!;
+                  scaleMin -= bucketSize!;
                 } else if (dataRef.current?.yLayout === HeatmapCellLayout.ge) {
-                  dataMax += bucketSize!;
+                  scaleMax += bucketSize!;
                 } else {
-                  dataMin -= bucketSize! / 2;
-                  dataMax += bucketSize! / 2;
+                  scaleMin -= bucketSize! / 2;
+                  scaleMax += bucketSize! / 2;
                 }
               } else {
                 // how to expand scale range if inferred non-regular or log buckets?
               }
 
               if (!isOrdianalY) {
-                dataMin = explicitMin ?? dataMin;
-                dataMax = explicitMax ?? dataMax;
+                scaleMin = explicitMin ?? scaleMin;
+                scaleMax = explicitMax ?? scaleMax;
               }
             }
 
-            return [dataMin, dataMax];
+            return [scaleMin, scaleMax];
           },
   });
 