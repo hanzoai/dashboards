grafana/grafana

Alerting: Define rules.alerting.grafana.app/v0alpha1 AlertRule and RecordingRule
The first version of the app platform apis for alerting rules, including AlertRule and RecordingRule definitions.

Co-authored-by: William Wernert <william.wernert@grafana.com>

apps/alerting/rules/pkg/apis/alerting/v0alpha1/alertrule_spec_gen.go
@@ -1,0 +1,117 @@
+// Code generated - EDITING IS FUTILE. DO NOT EDIT.
+
+package v0alpha1
+
+// TODO: validate that only one can specify source=true
+// & struct.MinFields(1) This doesn't work in Cue <v0.12.0 as per
+// +k8s:openapi-gen=true
+type AlertRuleQueryMap map[string]AlertRuleQuery
+
+// +k8s:openapi-gen=true
+type AlertRuleQuery struct {
+	QueryType         string                      `json:"queryType"`
+	RelativeTimeRange *AlertRuleRelativeTimeRange `json:"relativeTimeRange,omitempty"`
+	DatasourceUID     AlertRuleDatasourceUID      `json:"datasourceUID"`
+	Model             interface{}                 `json:"model"`
+	Source            *bool                       `json:"source,omitempty"`
+}
+
+// NewAlertRuleQuery creates a new AlertRuleQuery object.
+func NewAlertRuleQuery() *AlertRuleQuery {
+	return &AlertRuleQuery{}
+}
+
+// +k8s:openapi-gen=true
+type AlertRuleRelativeTimeRange struct {
+	From AlertRulePromDurationWMillis `json:"from"`
+	To   AlertRulePromDurationWMillis `json:"to"`
+}
+
+// NewAlertRuleRelativeTimeRange creates a new AlertRuleRelativeTimeRange object.
+func NewAlertRuleRelativeTimeRange() *AlertRuleRelativeTimeRange {
+	return &AlertRuleRelativeTimeRange{}
+}
+
+// +k8s:openapi-gen=true
+type AlertRulePromDurationWMillis string
+
+// TODO(@moustafab): validate regex for datasource UID
+// +k8s:openapi-gen=true
+type AlertRuleDatasourceUID string
+
+// +k8s:openapi-gen=true
+type AlertRuleIntervalTrigger struct {
+	Interval AlertRulePromDuration `json:"interval"`
+}
+
+// NewAlertRuleIntervalTrigger creates a new AlertRuleIntervalTrigger object.
+func NewAlertRuleIntervalTrigger() *AlertRuleIntervalTrigger {
+	return &AlertRuleIntervalTrigger{}
+}
+
+// +k8s:openapi-gen=true
+type AlertRulePromDuration string
+
+// TODO(@moustafab): validate regex for mute time interval ref
+// +k8s:openapi-gen=true
+type AlertRuleMuteTimeIntervalRef string
+
+// TODO(@moustafab): validate regex for active time interval ref
+// +k8s:openapi-gen=true
+type AlertRuleActiveTimeIntervalRef string
+
+// +k8s:openapi-gen=true
+type AlertRuleTemplateString string
+
+// +k8s:openapi-gen=true
+type AlertRuleSpec struct {
+	Title                       string                                     `json:"title"`
+	Data                        AlertRuleQueryMap                          `json:"data"`
+	Paused                      *bool                                      `json:"paused,omitempty"`
+	Trigger                     AlertRuleIntervalTrigger                   `json:"trigger"`
+	NoDataState                 string                                     `json:"noDataState"`
+	ExecErrState                string                                     `json:"execErrState"`
+	For                         *string                                    `json:"for,omitempty"`
+	KeepFiringFor               *string                                    `json:"keepFiringFor,omitempty"`
+	MissingSeriesEvalsToResolve *int64                                     `json:"missingSeriesEvalsToResolve,omitempty"`
+	NotificationSettings        *AlertRuleV0alpha1SpecNotificationSettings `json:"notificationSettings,omitempty"`
+	Annotations                 map[string]AlertRuleTemplateString         `json:"annotations,omitempty"`
+	Labels                      map[string]AlertRuleTemplateString         `json:"labels,omitempty"`
+	PanelRef                    *AlertRuleV0alpha1SpecPanelRef             `json:"panelRef,omitempty"`
+}
+
+// NewAlertRuleSpec creates a new AlertRuleSpec object.
+func NewAlertRuleSpec() *AlertRuleSpec {
+	return &AlertRuleSpec{
+		Trigger:      *NewAlertRuleIntervalTrigger(),
+		NoDataState:  "NoData",
+		ExecErrState: "Error",
+	}
+}
+
+// +k8s:openapi-gen=true
+type AlertRuleV0alpha1SpecNotificationSettings struct {
+	Receiver            string                           `json:"receiver"`
+	GroupBy             []string                         `json:"groupBy,omitempty"`
+	GroupWait           *string                          `json:"groupWait,omitempty"`
+	GroupInterval       *string                          `json:"groupInterval,omitempty"`
+	RepeatInterval      *string                          `json:"repeatInterval,omitempty"`
+	MuteTimeIntervals   []AlertRuleMuteTimeIntervalRef   `json:"muteTimeIntervals,omitempty"`
+	ActiveTimeIntervals []AlertRuleActiveTimeIntervalRef `json:"activeTimeIntervals,omitempty"`
+}
+
+// NewAlertRuleV0alpha1SpecNotificationSettings creates a new AlertRuleV0alpha1SpecNotificationSettings object.
+func NewAlertRuleV0alpha1SpecNotificationSettings() *AlertRuleV0alpha1SpecNotificationSettings {
+	return &AlertRuleV0alpha1SpecNotificationSettings{}
+}
+
+// +k8s:openapi-gen=true
+type AlertRuleV0alpha1SpecPanelRef struct {
+	DashboardUID string `json:"dashboardUID"`
+	PanelID      int64  `json:"panelID"`
+}
+
+// NewAlertRuleV0alpha1SpecPanelRef creates a new AlertRuleV0alpha1SpecPanelRef object.
+func NewAlertRuleV0alpha1SpecPanelRef() *AlertRuleV0alpha1SpecPanelRef {
+	return &AlertRuleV0alpha1SpecPanelRef{}
+}

apps/alerting/rules/pkg/app/app.go
@@ -1,0 +1,43 @@
+package app
+
+import (
+	"context"
+
+	"github.com/grafana/grafana-app-sdk/app"
+	"github.com/grafana/grafana-app-sdk/logging"
+	"github.com/grafana/grafana-app-sdk/simple"
+
+	"github.com/grafana/grafana/apps/alerting/rules/pkg/apis"
+)
+
+func New(cfg app.Config) (app.App, error) {
+	managedKinds := make([]simple.AppManagedKind, 0)
+	for _, kinds := range apis.GetKinds() {
+		for _, kind := range kinds {
+			managedKinds = append(managedKinds, simple.AppManagedKind{Kind: kind})
+		}
+	}
+
+	c := simple.AppConfig{
+		Name:       "alerting.notification",
+		KubeConfig: cfg.KubeConfig,
+		InformerConfig: simple.AppInformerConfig{
+			ErrorHandler: func(ctx context.Context, err error) {
+				logging.DefaultLogger.With("error", err).Error("Informer processing error")
+			},
+		},
+		ManagedKinds: managedKinds,
+	}
+
+	a, err := simple.NewApp(c)
+	if err != nil {
+		return nil, err
+	}
+
+	err = a.ValidateManifest(cfg.ManifestData)
+	if err != nil {
+		return nil, err
+	}
+
+	return a, nil
+}

apps/alerting/rules/plugin/src/generated/alertrule/v0alpha1/types.spec.gen.ts
@@ -1,0 +1,102 @@
+// Code generated - EDITING IS FUTILE. DO NOT EDIT.
+
+// TODO: validate that only one can specify source=true
+// & struct.MinFields(1) This doesn't work in Cue <v0.12.0 as per
+export type QueryMap = Record<string, Query>;
+
+export const defaultQueryMap = (): QueryMap => ({});
+
+export interface Query {
+	queryType: string;
+	relativeTimeRange?: RelativeTimeRange;
+	datasourceUID: DatasourceUID;
+	model: any;
+	source?: boolean;
+}
+
+export const defaultQuery = (): Query => ({
+	queryType: "",
+	datasourceUID: defaultDatasourceUID(),
+	model: {},
+});
+
+export interface RelativeTimeRange {
+	from: PromDurationWMillis;
+	to: PromDurationWMillis;
+}
+
+export const defaultRelativeTimeRange = (): RelativeTimeRange => ({
+	from: defaultPromDurationWMillis(),
+	to: defaultPromDurationWMillis(),
+});
+
+export type PromDurationWMillis = string;
+
+export const defaultPromDurationWMillis = (): PromDurationWMillis => ("");
+
+// TODO(@moustafab): validate regex for datasource UID
+export type DatasourceUID = string;
+
+export const defaultDatasourceUID = (): DatasourceUID => ("");
+
+export interface IntervalTrigger {
+	interval: PromDuration;
+}
+
+export const defaultIntervalTrigger = (): IntervalTrigger => ({
+	interval: defaultPromDuration(),
+});
+
+export type PromDuration = string;
+
+export const defaultPromDuration = (): PromDuration => ("");
+
+// TODO(@moustafab): validate regex for mute time interval ref
+export type MuteTimeIntervalRef = string;
+
+export const defaultMuteTimeIntervalRef = (): MuteTimeIntervalRef => ("");
+
+// TODO(@moustafab): validate regex for active time interval ref
+export type ActiveTimeIntervalRef = string;
+
+export const defaultActiveTimeIntervalRef = (): ActiveTimeIntervalRef => ("");
+
+export type TemplateString = string;
+
+export const defaultTemplateString = (): TemplateString => ("");
+
+export interface Spec {
+	title: string;
+	data: QueryMap;
+	paused?: boolean;
+	trigger: IntervalTrigger;
+	noDataState: string;
+	execErrState: string;
+	for?: string;
+	keepFiringFor?: string;
+	missingSeriesEvalsToResolve?: number;
+	notificationSettings?: {
+		receiver: string;
+		groupBy?: string[];
+		groupWait?: string;
+		groupInterval?: string;
+		repeatInterval?: string;
+		muteTimeIntervals?: MuteTimeIntervalRef[];
+		activeTimeIntervals?: ActiveTimeIntervalRef[];
+	};
+	annotations?: Record<string, TemplateString>;
+	labels?: Record<string, TemplateString>;
+	panelRef?: {
+		dashboardUID: string;
+		panelID: number;
+	};
+}
+
+export const defaultSpec = (): Spec => ({
+	title: "",
+	data: defaultQueryMap(),
+	trigger: defaultIntervalTrigger(),
+	noDataState: "NoData",
+	execErrState: "Error",
+});
+

hanzoai/dashboards

apps/alerting/rules/pkg/apis/alerting/v0alpha1/alertrule_spec_gen.go
@@ -1,0 +1,114 @@
+// Code generated - EDITING IS FUTILE. DO NOT EDIT.
+
+package v0alpha1
+
+// TODO: validate that only one can specify source=true
+// & struct.MinFields(1) This doesn't work in Cue <v0.12.0 as per
+// +k8s:openapi-gen=true
+type AlertRuleQueryMap map[string]AlertRuleQuery
+
+// TODO: come up with a better name for this. We have expression type things and data source queries
+// +k8s:openapi-gen=true
+type AlertRuleQuery struct {
+	// TODO: consider making this optional, with the nil value meaning "__expr__" (i.e. expression query)
+	QueryType         string                      `json:"queryType"`
+	RelativeTimeRange *AlertRuleRelativeTimeRange `json:"relativeTimeRange,omitempty"`
+	DatasourceUID     AlertRuleDatasourceUID      `json:"datasourceUID"`
+	Model             interface{}                 `json:"model"`
+	Source            *bool                       `json:"source,omitempty"`
+}
+
+// NewAlertRuleQuery creates a new AlertRuleQuery object.
+func NewAlertRuleQuery() *AlertRuleQuery {
+	return &AlertRuleQuery{}
+}
+
+// +k8s:openapi-gen=true
+type AlertRuleRelativeTimeRange struct {
+	From AlertRulePromDurationWMillis `json:"from"`
+	To   AlertRulePromDurationWMillis `json:"to"`
+}
+
+// NewAlertRuleRelativeTimeRange creates a new AlertRuleRelativeTimeRange object.
+func NewAlertRuleRelativeTimeRange() *AlertRuleRelativeTimeRange {
+	return &AlertRuleRelativeTimeRange{}
+}
+
+// +k8s:openapi-gen=true
+type AlertRulePromDurationWMillis string
+
+// +k8s:openapi-gen=true
+type AlertRuleDatasourceUID string
+
+// +k8s:openapi-gen=true
+type AlertRuleIntervalTrigger struct {
+	Interval AlertRulePromDuration `json:"interval"`
+}
+
+// NewAlertRuleIntervalTrigger creates a new AlertRuleIntervalTrigger object.
+func NewAlertRuleIntervalTrigger() *AlertRuleIntervalTrigger {
+	return &AlertRuleIntervalTrigger{}
+}
+
+// +k8s:openapi-gen=true
+type AlertRulePromDuration string
+
+// TODO(@moustafab): validate regex for time interval ref
+// +k8s:openapi-gen=true
+type AlertRuleTimeIntervalRef string
+
+// +k8s:openapi-gen=true
+type AlertRuleTemplateString string
+
+// +k8s:openapi-gen=true
+type AlertRuleSpec struct {
+	Title                       string                                     `json:"title"`
+	Data                        AlertRuleQueryMap                          `json:"data"`
+	Paused                      *bool                                      `json:"paused,omitempty"`
+	Trigger                     AlertRuleIntervalTrigger                   `json:"trigger"`
+	NoDataState                 string                                     `json:"noDataState"`
+	ExecErrState                string                                     `json:"execErrState"`
+	For                         *string                                    `json:"for,omitempty"`
+	KeepFiringFor               *string                                    `json:"keepFiringFor,omitempty"`
+	MissingSeriesEvalsToResolve *int64                                     `json:"missingSeriesEvalsToResolve,omitempty"`
+	NotificationSettings        *AlertRuleV0alpha1SpecNotificationSettings `json:"notificationSettings,omitempty"`
+	Annotations                 map[string]AlertRuleTemplateString         `json:"annotations,omitempty"`
+	Labels                      map[string]AlertRuleTemplateString         `json:"labels,omitempty"`
+	PanelRef                    *AlertRuleV0alpha1SpecPanelRef             `json:"panelRef,omitempty"`
+}
+
+// NewAlertRuleSpec creates a new AlertRuleSpec object.
+func NewAlertRuleSpec() *AlertRuleSpec {
+	return &AlertRuleSpec{
+		Trigger:      *NewAlertRuleIntervalTrigger(),
+		NoDataState:  "NoData",
+		ExecErrState: "Error",
+	}
+}
+
+// +k8s:openapi-gen=true
+type AlertRuleV0alpha1SpecNotificationSettings struct {
+	Receiver            string                     `json:"receiver"`
+	GroupBy             []string                   `json:"groupBy,omitempty"`
+	GroupWait           *AlertRulePromDuration     `json:"groupWait,omitempty"`
+	GroupInterval       *AlertRulePromDuration     `json:"groupInterval,omitempty"`
+	RepeatInterval      *AlertRulePromDuration     `json:"repeatInterval,omitempty"`
+	MuteTimeIntervals   []AlertRuleTimeIntervalRef `json:"muteTimeIntervals,omitempty"`
+	ActiveTimeIntervals []AlertRuleTimeIntervalRef `json:"activeTimeIntervals,omitempty"`
+}
+
+// NewAlertRuleV0alpha1SpecNotificationSettings creates a new AlertRuleV0alpha1SpecNotificationSettings object.
+func NewAlertRuleV0alpha1SpecNotificationSettings() *AlertRuleV0alpha1SpecNotificationSettings {
+	return &AlertRuleV0alpha1SpecNotificationSettings{}
+}
+
+// +k8s:openapi-gen=true
+type AlertRuleV0alpha1SpecPanelRef struct {
+	DashboardUID string `json:"dashboardUID"`
+	PanelID      int64  `json:"panelID"`
+}
+
+// NewAlertRuleV0alpha1SpecPanelRef creates a new AlertRuleV0alpha1SpecPanelRef object.
+func NewAlertRuleV0alpha1SpecPanelRef() *AlertRuleV0alpha1SpecPanelRef {
+	return &AlertRuleV0alpha1SpecPanelRef{}
+}

apps/alerting/rules/pkg/app/app.go
@@ -1,0 +1,43 @@
+package app
+
+import (
+	"context"
+
+	"github.com/grafana/grafana-app-sdk/app"
+	"github.com/grafana/grafana-app-sdk/logging"
+	"github.com/grafana/grafana-app-sdk/simple"
+
+	"github.com/grafana/grafana/apps/alerting/rules/pkg/apis"
+)
+
+func New(cfg app.Config) (app.App, error) {
+	managedKinds := make([]simple.AppManagedKind, 0)
+	for _, kinds := range apis.GetKinds() {
+		for _, kind := range kinds {
+			managedKinds = append(managedKinds, simple.AppManagedKind{Kind: kind})
+		}
+	}
+
+	c := simple.AppConfig{
+		Name:       "alerting.rules",
+		KubeConfig: cfg.KubeConfig,
+		InformerConfig: simple.AppInformerConfig{
+			ErrorHandler: func(ctx context.Context, err error) {
+				logging.DefaultLogger.With("error", err).Error("Informer processing error")
+			},
+		},
+		ManagedKinds: managedKinds,
+	}
+
+	a, err := simple.NewApp(c)
+	if err != nil {
+		return nil, err
+	}
+
+	err = a.ValidateManifest(cfg.ManifestData)
+	if err != nil {
+		return nil, err
+	}
+
+	return a, nil
+}

apps/alerting/rules/plugin/src/generated/alertrule/v0alpha1/types.spec.gen.ts
@@ -1,0 +1,98 @@
+// Code generated - EDITING IS FUTILE. DO NOT EDIT.
+
+// TODO: validate that only one can specify source=true
+// & struct.MinFields(1) This doesn't work in Cue <v0.12.0 as per
+export type QueryMap = Record<string, Query>;
+
+export const defaultQueryMap = (): QueryMap => ({});
+
+// TODO: come up with a better name for this. We have expression type things and data source queries
+export interface Query {
+	// TODO: consider making this optional, with the nil value meaning "__expr__" (i.e. expression query)
+	queryType: string;
+	relativeTimeRange?: RelativeTimeRange;
+	datasourceUID: DatasourceUID;
+	model: any;
+	source?: boolean;
+}
+
+export const defaultQuery = (): Query => ({
+	queryType: "",
+	datasourceUID: defaultDatasourceUID(),
+	model: {},
+});
+
+export interface RelativeTimeRange {
+	from: PromDurationWMillis;
+	to: PromDurationWMillis;
+}
+
+export const defaultRelativeTimeRange = (): RelativeTimeRange => ({
+	from: defaultPromDurationWMillis(),
+	to: defaultPromDurationWMillis(),
+});
+
+export type PromDurationWMillis = string;
+
+export const defaultPromDurationWMillis = (): PromDurationWMillis => ("");
+
+export type DatasourceUID = string;
+
+export const defaultDatasourceUID = (): DatasourceUID => ("");
+
+export interface IntervalTrigger {
+	interval: PromDuration;
+}
+
+export const defaultIntervalTrigger = (): IntervalTrigger => ({
+	interval: defaultPromDuration(),
+});
+
+export type PromDuration = string;
+
+export const defaultPromDuration = (): PromDuration => ("");
+
+// TODO(@moustafab): validate regex for time interval ref
+export type TimeIntervalRef = string;
+
+export const defaultTimeIntervalRef = (): TimeIntervalRef => ("");
+
+export type TemplateString = string;
+
+export const defaultTemplateString = (): TemplateString => ("");
+
+export interface Spec {
+	title: string;
+	data: QueryMap;
+	paused?: boolean;
+	trigger: IntervalTrigger;
+	noDataState: string;
+	execErrState: string;
+	for?: string;
+	keepFiringFor?: string;
+	missingSeriesEvalsToResolve?: number;
+	notificationSettings?: {
+		receiver: string;
+		groupBy?: string[];
+		groupWait?: PromDuration;
+		groupInterval?: PromDuration;
+		repeatInterval?: PromDuration;
+		muteTimeIntervals?: TimeIntervalRef[];
+		activeTimeIntervals?: TimeIntervalRef[];
+	};
+	annotations?: Record<string, TemplateString>;
+	labels?: Record<string, TemplateString>;
+	panelRef?: {
+		dashboardUID: string;
+		panelID: number;
+	};
+}
+
+export const defaultSpec = (): Spec => ({
+	title: "",
+	data: defaultQueryMap(),
+	trigger: defaultIntervalTrigger(),
+	noDataState: "NoData",
+	execErrState: "Error",
+});
+